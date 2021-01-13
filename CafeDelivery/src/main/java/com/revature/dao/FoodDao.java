abbdc4f43b19e9fb5ec68770667aaa58dc23b860
agee11 <a.gee5@yahoo.com>
2021-01-12 19:22:33 -0800

    Add implementation to Dao and minor refactor to Dao interface to utilize hibernate methods

--- CafeDelivery/src/main/java/com/revature/dao/FoodDao.java
+++ CafeDelivery/src/main/java/com/revature/dao/FoodDao.java
@@ -1,5 +1,21 @@
 package com.revature.dao;
 
-public interface FoodDao {
+import java.util.List;
 
+import com.revature.model.Food;
+
+public interface FoodDao {
+	
+	//Create methods
+	public void insertNewFood(Food f);
+	
+	//Read methods
+	public Food getFoodById(int id);
+	public List<Food> getAllFood();
+	
+	//Update methods
+	public void updateFood(Food f);
+	
+	//Delete methods
+	public void deleteFood(Food f);
 }
--- CafeDelivery/src/main/java/com/revature/util/HibernateUtil.java
+++ CafeDelivery/src/main/java/com/revature/util/HibernateUtil.java
@@ -6,10 +6,22 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 	
+	//Utilizes the singleton design pattern
+	//Be sure to close session when done.
 	private static SessionFactory sf = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-	
+	private static Session ses;
 	public static Session getSession() {
-		return sf.openSession();
+		if(ses == null) {
+			return ses = sf.getCurrentSession();
+		}else {
+			return ses;
+		}
+		
+	}
+	
+	public static void closeSession() {
+		ses.close();
+		ses = null;
 	}
 
 }
