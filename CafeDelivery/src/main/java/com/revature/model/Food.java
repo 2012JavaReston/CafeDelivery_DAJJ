1e69e3776a3d049a7ee95c0216f2eac3145ac2a9
agee11 <a.gee5@yahoo.com>
2021-01-12 18:33:48 -0800

    Add details to model objects along with annotations for hibernate

--- CafeDelivery/src/main/java/com/revature/model/Food.java
+++ CafeDelivery/src/main/java/com/revature/model/Food.java
@@ -1,5 +1,60 @@
 package com.revature.model;
 
-public class Food {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "food_table")
+public class Food {
+	
+	@Id
+	@Column(name = "food_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name = "food_name", unique = true, nullable = false)
+	private String name;
+	
+	@Column(name = "price", nullable = false)
+	private double price;
+	
+	
+	public Food(int id, String name, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+	public Food() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Food [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
 }
